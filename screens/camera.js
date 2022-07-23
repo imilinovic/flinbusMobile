4da3db774e05cd95f3d0776006a52645ee61124b
Filip Beleta <filip.beleta@gmail.com>
2022-07-23 17:12:18 +0200

    Added Camera Screen

--- screens/camera.js
+++ screens/camera.js
@@ -2,20 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 
-export default function kamera() {
+export default function Kamera() {
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(type === CameraType.back ? CameraType.front : CameraType.back);
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+    <View>
+      <Camera>
+    </Camera>
     </View>
   );
 }
