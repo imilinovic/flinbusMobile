e3bd4eac9d4952a2a966c491dac0f5b30f0350f7
Filip Beleta <filip.beleta@gmail.com>
2022-07-23 16:25:08 +0200

    Temp progress.

---
+++ screens/camera.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+
+export default function kamera() {
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(type === CameraType.back ? CameraType.front : CameraType.back);
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
