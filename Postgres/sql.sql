f2853fd9ab7b41b8d542d0622decbb4e9a21811d
Ivan Milinovic <ivan.milinovic@phishar.com>
2022-07-23 22:37:30 +0200

    postgres update

--- Postgres/sql.sql
+++ Postgres/sql.sql
@@ -8,13 +8,15 @@ CREATE TABLE profile
   profilePoints INT NOT NULL,
   profileEmail VARCHAR(50) NOT NULL,
   profileTotalPoints INT NOT NULL,
+  profileToken CHAR(16) NOT NULL,
   PRIMARY KEY (profileUsername),
-  UNIQUE (profileEmail)
+  UNIQUE (profileEmail),
+  UNIQUE(profileToken)
 );
 
 CREATE TABLE reward
 (
-  rewardName VARCHAR(50) NOT NULL,
+  rewardName VARCHAR(30) NOT NULL,
   rewardCost INT NOT NULL,
   rewardId INT NOT NULL,
   rewardInfo VARCHAR(300) NOT NULL,
@@ -22,44 +24,26 @@ CREATE TABLE reward
   PRIMARY KEY (rewardId)
 );
 
-CREATE TABLE location
+CREATE TABLE event
 (
-  locationLongitude NUMERIC(9, 3) NOT NULL,
-  locationLatitude NUMERIC(9, 3) NOT NULL,
-  PRIMARY KEY (locationLongitude, locationLatitude)
+  eventName VARCHAR(50) NOT NULL,
+  eventId INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  eventInfo VARCHAR(300) NOT NULL,
+  eventTime INT NOT NULL,
+  eventLatitude NUMERIC(9, 6) NOT NULL,
+  eventLongitude NUMERIC(9, 6) NOT NULL,
+  profileUsername VARCHAR(30) NOT NULL,
+  FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE trashReport
 (
-  reportPath VARCHAR(300) NOT NULL,
-  reportId INT NOT NULL,
+  reportPath VARCHAR(300),
+  reportId INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  reportLongitude NUMERIC(9, 6) NOT NULL,
+  reportLatitude NUMERIC(9, 6) NOT NULL,
+  reportType VARCHAR(20) NOT NULL,
   profileUsername VARCHAR(30) NOT NULL,
-  locationLongitude NUMERIC(9, 3) NOT NULL,
-  locationLatitude NUMERIC(9, 3) NOT NULL,
-  PRIMARY KEY (reportId),
-  FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (locationLongitude, locationLatitude) REFERENCES location(locationLongitude, locationLatitude) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE buys
-(
-  profileUsername VARCHAR(30) NOT NULL,
-  rewardId INT NOT NULL,
-  PRIMARY KEY (profileUsername, rewardId),
-  FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (rewardId) REFERENCES reward(rewardId) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE event
-(
-  eventName VARCHAR(50) NOT NULL,
-  eventId INT NOT NULL,
-  eventInfo VARCHAR(300) NOT NULL,
-  locationLongitude NUMERIC(9, 3) NOT NULL,
-  locationLatitude NUMERIC(9, 3) NOT NULL,
-  profileUsername VARCHAR(30) NOT NULL,
-  PRIMARY KEY (eventId),
-  FOREIGN KEY (locationLongitude, locationLatitude) REFERENCES location(locationLongitude, locationLatitude) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -71,4 +55,13 @@ CREATE TABLE participates
   PRIMARY KEY (profileUsername, eventId),
   FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (eventId) REFERENCES event(eventId) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE buys
+(
+  profileUsername VARCHAR(30) NOT NULL,
+  rewardId INT NOT NULL,
+  PRIMARY KEY (profileUsername, rewardId),
+  FOREIGN KEY (profileUsername) REFERENCES profile(profileUsername) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (rewardId) REFERENCES reward(rewardId) ON DELETE CASCADE ON UPDATE CASCADE
 );
